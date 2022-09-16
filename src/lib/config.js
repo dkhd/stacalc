c454b1a5feceefe5a8044f38233d43532f7e9db9
Diky Hadna <hi@hadna.space>
2022-09-16 07:36:50 +0700

    add: calculator smart contract

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,7 @@
 import { StacksTestnet } from "@stacks/network";
 
 const Config = {
-  contractName: "calculator",
+  contractName: "stacalc",
   contractAddress: "ST5K05SFTN5MZ17HTMA9HXSMHF6H301SYQNBD9E9",
 };
 
